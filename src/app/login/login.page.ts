e647930e77b542d239f572a23fc6168431ab7bba
raulli96 <ramswal.raulli96@gmail.com>
2020-06-25 11:17:50 +0530

    first commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router:Router) { }
+  UserName: string;
+  Password: string;  
+
+  ngOnInit() {
+  }
+
+  LogMeIn(){
+
+    if(this.UserName=="admin"&& this.Password=="admin123")
+   {
+    this.router.navigate(['/home'])
+   }  
+  }
+}
+
