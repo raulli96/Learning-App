608b9027e96fa13ef6af3cc894f79e2b29b59253
raulli96 <Ranswal.raulli96@gmail.com>
2020-06-29 19:41:07 +0530

    validation

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
+  
 })
 export class SignupPage implements OnInit {
+ get FirstName(){return this.myform.get('FirstName')}
+ get LastName(){return this.myform.get('LastName')}
+ get UserName(){return this.myform.get('UserName')}
+ get PhoneNo(){return this.myform.get('PhoneNo')}
+ get Email(){return this.myform.get('Email')}
+ get Password(){return this.myform.get('Password')}
 
-  constructor() { }
+myform=this.mybuilder.group({
+FirstName:['',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+LastName:['',[Validators.required, Validators.minLength(5), Validators.maxLength(15)]],
+UserName:['',[Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+PhoneNo:['',[Validators.required,Validators.maxLength(10)]],
+Email:['',[Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.email]],
+Password:['',[Validators.required, Validators.minLength(3), Validators.maxLength(10)]],
+
+}
+)
+
+  constructor(private  mybuilder:FormBuilder ) { }
 
   ngOnInit() {
   }
